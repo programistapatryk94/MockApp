1e6ed18ffd2737188cde8349ccb53e6b36b5f834
Patryk <programistapatryk94@gmail.com>
2025-07-18 16:37:35 +0200

    Change material initialization

--- mockapp-frontend/src/app/projects/create-or-update-project/create-or-update-project.component.ts
+++ mockapp-frontend/src/app/projects/create-or-update-project/create-or-update-project.component.ts
@@ -38,6 +38,7 @@ export interface CreateOrUpdateProjectDialogData {
 @Component({
   selector: 'app-create-or-update-project',
   templateUrl: './create-or-update-project.component.html',
+  styleUrls: ['./create-or-update-project.component.scss'],
   imports: [
     MatDialogModule,
     MatFormFieldModule,
